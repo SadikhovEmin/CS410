90376a1bdf3aedc0a7c6d80e5ecb84c801282559
SadikhovEmin <eemin.sadykhov@gmail.com>
2022-01-06 19:27:28 +0300

    Algorithm added. Not working yet

--- Project 3/src/Main.java
+++ Project 3/src/Main.java
@@ -7,19 +7,19 @@ public class Main {
         TuringMachine turingMachine = new TuringMachine(readFile);
         turingMachine.simulate();
 
-        System.out.println("readFile.numberOfInputAlphabets = " + readFile.numberOfInputAlphabets);
-        System.out.println("readFile.numberOfTapeAlphabets = " + readFile.numberOfTapeAlphabets);
-        System.out.println("readFile.numberOfStates = " + readFile.numberOfStates);
-        System.out.println("readFile.states = " + readFile.states);
-        System.out.println("readFile.startState = " + readFile.startState);
-        System.out.println("readFile.acceptState = " + readFile.acceptState);
-        System.out.println("readFile.rejectState = " + readFile.rejectState);
-        System.out.println("readFile.blankSymbol = " + readFile.blankSymbol);
-        System.out.println("readFile.tapeAlphabets = " + readFile.tapeAlphabets);
-        System.out.println("readFile.inputAlphabet = " + readFile.inputAlphabet);
-        System.out.println("readFile.transitions = " + readFile.transitions);
-        System.out.println("readFile.stringsToDetect = " + readFile.stringsToDetect);
-
-
+//        System.out.println("readFile.numberOfInputAlphabets = " + readFile.numberOfInputAlphabets);
+//        System.out.println("readFile.numberOfTapeAlphabets = " + readFile.numberOfTapeAlphabets);
+//        System.out.println("readFile.numberOfStates = " + readFile.numberOfStates);
+//        System.out.println("readFile.states = " + readFile.states);
+//        System.out.println("readFile.startState = " + readFile.startState);
+//        System.out.println("readFile.acceptState = " + readFile.acceptState);
+//        System.out.println("readFile.rejectState = " + readFile.rejectState);
+//        System.out.println("readFile.blankSymbol = " + readFile.blankSymbol);
+//        System.out.println("readFile.tapeAlphabets = " + readFile.tapeAlphabets);
+//        System.out.println("readFile.inputAlphabet = " + readFile.inputAlphabet);
+//        System.out.println("readFile.transitions = " + readFile.transitions);
+//        System.out.println("readFile.stringsToDetect = " + readFile.stringsToDetect);
+//
+//
     }
 }
--- Project 3/src/TuringMachine.java
+++ Project 3/src/TuringMachine.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TuringMachine {
@@ -11,6 +12,38 @@ public class TuringMachine {
     }
 
     public void simulate() {
-        System.out.println(tape);
+        String currentState = readFile.startState;
+        int tapePointer = 0;        // This keeps the track of the movements on the tape
+
+        for (int i = 0; i < readFile.stringsToDetect.size(); i++) {     // Iterates over the input
+            tape = new ArrayList<>();
+            for (int j = 0; j < readFile.stringsToDetect.get(i).length(); j++)        // Adds whole input to tape
+                tape.add(readFile.stringsToDetect.get(i).charAt(j) + "");
+
+            tape.add(readFile.blankSymbol);
+            System.out.println("Tape " + tape);
+
+            for (Transition t : readFile.transitions) {
+                if (currentState.equals(t.fromState) && tape.get(tapePointer).equals(t.variable)) {
+                    currentState = t.toState;
+                    tape.set(tapePointer, t.writeToTape);
+
+                    if (t.move.equals("R"))
+                        tapePointer++;
+                    else if (t.move.equals("L") && tapePointer != 0)        // Check that leftmost is still 0
+                        tapePointer--;
+
+                    System.out.println("Current " + currentState);
+                    break;
+                }
+            }
+
+            if (currentState.equals(readFile.acceptState))
+                System.out.println("Accepted");
+            else if (currentState.equals(readFile.rejectState))
+                System.out.println("Reject");
+
+//            System.out.println("tape = " + tape);
+        }
     }
 }
